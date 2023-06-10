977606c49ebd282f37610e08718615cbc3b175e3
Maciej Górnik <maciekgornik@student.agh.edu.pl>
2023-06-10 22:13:29 +0200

    added some things and todo

--- website/models.py
+++ website/models.py
@@ -44,34 +44,3 @@ class OnlineUser(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     last_seen = db.Column(db.DateTime(timezone=True), default=func.now())
 
-# from . import db
-# from flask_login import UserMixin
-# from sqlalchemy.sql import func
-
-
-# # ''' TODO: stworzenie klasy wiadomości (Message)
-# #         - przemyśleć w jaki sposób przechowujemy widomości i jak bedziemy je odczytywac i wyświetlać
-# #     TODO: stworzenie klasy czatu (ChatRoom)
-# #     - zawiera w sobie id użytkowników piszących ze soba (user_id_1, user_id_2),
-# #     - dziedziczy z klasy Message (db.relationship)??? jakies połącznie miedzy pokojem a wiadomościami,
-# #     - port na jakim utworzony jest pokój???)
-# #     TODO: dodanie klasy (Server)???
-# #     - uruchomione czaty (porty do chatroomów)
-# #     - aktywni/zalogowani użytkownicy
-# #     TODO: przemyśleć jak zrobić tego moderatora
-# # '''
-
-
-# class Note(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     data = db.Column(db.String(10000))
-#     date = db.Column(db.DateTime(timezone=True), default=func.now())
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(150), unique=True)
-#     password = db.Column(db.String(150))
-#     first_name = db.Column(db.String(150))
-#     notes = db.relationship('Note')
